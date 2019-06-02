956ab727da346279101af8d3e02737a8f015a164
tvijay-akamai <51293194+tvijay-akamai@users.noreply.github.com>
2019-06-03 00:50:24 +0530

    Add files via upload

---
+++ food-order-app/README.md
@@ -0,0 +1,3 @@
+Burger Builder React App with help of Maximilian Schwarzmüller's React 16 course on Udemy
+
+https://bosilovatzburgerbuilder.herokuapp.com/
---
+++ food-order-app/src/store/reducers/burgerBuilder.js
@@ -0,0 +1,74 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../shared/utility';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4,
+    error: false,
+    building: false
+};
+
+const INGREDIENT_PRICES = {
+    lettuce: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+    onion: 0.3,
+    tomato: 0.3,
+    pickles: 0.2,
+};
+
+const addIngredient = ( state, action ) => {
+    const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
+    const updatedIngredients = updateObject( state.ingredients, updatedIngredient );
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    }
+    return updateObject( state, updatedState );
+};
+
+const removeIngredient = (state, action) => {
+    const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 }
+    const updatedIngs = updateObject( state.ingredients, updatedIng );
+    const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        building: true
+    }
+    return updateObject( state, updatedSt );
+};
+
+const setIngredients = (state, action) => {
+    return updateObject( state, {
+        ingredients: {
+            lettuce: action.ingredients.lettuce,
+            bacon: action.ingredients.bacon,
+            onion: action.ingredients.onion,
+            pickles: action.ingredients.pickles,
+            tomato: action.ingredients.tomato,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat          
+        },
+        totalPrice: 4,
+        error: false,
+        building: false
+    } );
+};
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject( state, { error: true } );
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case actionTypes.ADD_INGREDIENT: return addIngredient( state, action );
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);    
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+        default: return state;
+    }
+};
+
+export default reducer;
